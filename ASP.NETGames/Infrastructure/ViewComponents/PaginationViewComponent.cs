6fe83986fa452a6b8a780a72edf1ad457f0ab7c1
Romil <romil.masimov@gmail.com>
2019-11-17 01:34:04 +0400

    Pagination was added

---
+++ ASP.NETGames/Infrastructure/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using ASP.NETGames.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETGames.Infrastructure.ViewComponents
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string controller, string action, int page, int pageCount, Dictionary<string, string> routeData)
+        {
+            int firstPage = 1;
+            int lastPage = pageCount;
+            if (pageCount > 6)
+            {
+                lastPage = page + 3;
+                if (lastPage > pageCount)
+                    lastPage = pageCount;
+                firstPage = page - 3;
+                if (firstPage < 1)
+                    firstPage = 1;
+            }
+
+            var model = new PaginationViewModel
+            {
+                Controller = controller,
+                Action = action,
+                Page = page,
+                PageCount = pageCount,
+                FirstPage = firstPage,
+                LastPage = lastPage,
+                RouteData = routeData
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ ASP.NETGames/Models/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETGames.Models.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public int Page { get; set; }
+        public int PageCount { get; set; }
+        public int FirstPage { get; set; }
+        public int LastPage { get; set; }
+        public Dictionary<string, string> RouteData { get; set; }
+    }
+}
